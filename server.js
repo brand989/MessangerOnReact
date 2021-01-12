86d1fa42459eeb068be68485d803378dd5fa39a4
brand989 <brand989@gmail.com>
2021-01-12 16:00:58 +0300

    test bug server for herokku7

--- server.js
+++ server.js
@@ -3,23 +3,14 @@ const fs = require('fs')
 const app = express()
 const bodyParser = require('body-parser')
 let port = process.env.PORT || 2000
-
-app.use(express.static('./dist'))
-app.use(bodyParser.json())
-
-app.listen(port , () => {
-  console.log("Server started")
-})
+const path = require('path');
+const cool = require('cool-ascii-faces');
 
 
-app.get('/', (req, res) => {
-    let body = fs.readFileSync('./dist/index.html')
-
-    res.send(body)
-  })
-
-  app.get('/index.bundle.js', (req, res) => {
-    let body = fs.readFileSync('./dist/index.bundle.js')
-    
-    res.send(body)
-  })
+express()
+    .use(express.static(path.join(__dirname, './dist')))
+    .get('/', (req, res) => res.render('./dist/index.html'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(port , () => {
+        console.log("Server started")
+      })
