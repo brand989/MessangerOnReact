49a193c96e16e96747a84006c1c18666e8c8ecca
brand989 <brand989@gmail.com>
2021-01-12 15:19:38 +0300

    add port in app.listen for heroku new

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const fs = require('fs') 
 const app = express()
 const bodyParser = require('body-parser')
+let port = process.env.PORT || 2000
 
 app.use(express.static('./dist'))
 app.use(bodyParser.json())
 
-app.listen('8000', () => {
+app.listen(port , () => {
   console.log("Server started")
 })
